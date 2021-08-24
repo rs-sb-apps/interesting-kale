a3f4d530186fd35a6d4d09ca42ca348b956ccaa1
Ramon Snir <r@mon.dev>
2021-08-24 11:40:01 -0400

    cut withPrefix

--- src/utils/withPrefix.js
+++ src/utils/withPrefix.js
@@ -1,5 +1,4 @@
 const _ = require('lodash');
-const pathPrefix = require('../../content/data/config.json').path_prefix;
 
 export default function withPrefix(url) {
     if (!url) {
@@ -9,6 +8,6 @@ export default function withPrefix(url) {
     if (_.startsWith(url, '#') || _.startsWith(url, 'http://') || _.startsWith(url, 'https://')) {
         return url;
     }
-    const basePath = _.trim(pathPrefix, '/');
+    const basePath = _.trim('/', '/');
     return '/' + _.compact([basePath, _.trimStart(url, '/')]).join('/');
 }
